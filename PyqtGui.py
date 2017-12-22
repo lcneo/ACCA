4313ac84a82301abb7254860c9ddcc2a8eac5b9c
neo <33708128+lcneo@users.noreply.github.com>
2017-12-22 13:18:22 +0800

    Update PyqtGui.py

--- PyqtGui.py
+++ PyqtGui.py
@@ -14,8 +14,13 @@ class win(QWidget):
         QToolTip.setFont(QFont("sansSerif",10))
         #self.setToolTip('This is a <b>Qwidget</b> widget')
         #self.pbtn()
+        #mainw = QMW(self)
+
         self.pbtn()
         self.pqbtn()
+        self.qhbox()
+        #self.label()
+
         self.setWindowTitle("Icon")
         self.setWindowIcon(QIcon("web.png"))
         self.show()
@@ -31,11 +36,33 @@ class win(QWidget):
         qbtn.clicked.connect(QCoreApplication.instance().quit)
         qbtn.resize(qbtn.sizeHint())
         qbtn.move(20,62)
+
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+    def qhbox(self):
+
+        okButton = QPushButton('OK')
+        cancelButton = QPushButton("Cancel")
+        QuitButton = QPushButton("Quit")
+        QuitButton.clicked.connect(QCoreApplication.instance().quit)
+
+        okButton.setToolTip("Just a <b>OK</b> Button!")
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+        hbox.addWidget(QuitButton)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
     def closeEvent(self, event):
 
         reply = QMessageBox.question(self, '警告',
@@ -46,7 +73,15 @@ class win(QWidget):
             event.accept()
         else:
             event.ignore()
+    def label(self):
+        lb11 = QLabel('hello ', self)
+        lb11.move(115,110)
 
+        lb12 = QLabel('world ', self)
+        lb12.move(135, 140)
+
+        lb13 = QLabel('neo ', self)
+        lb13.move(155, 170)
 
 class QMW(QMainWindow):
     def __init__(self):
@@ -55,13 +90,15 @@ class QMW(QMainWindow):
 
     def initUI(self):
         self.statusBar().showMessage("hello neo")
-        self.setGeometry(300,300,300,200)
 
+        #self.setGeometry(300,300,300,200)
+        self.resize(400,600)
+        self.center()
         textEdit = QTextEdit()
         self.setCentralWidget(textEdit)
 
 
-        exitAction = QAction(QIcon("exit24.png"), 'Exit',self)
+        exitAction = QAction(QIcon(r"C:\Users\Administrator\Desktop\aa.jpg"), 'Exit',self)
         exitAction.setShortcut("Ctrl+Q")
         exitAction.setStatusTip("Exit application")
         exitAction.triggered.connect(self.close)
@@ -77,8 +114,14 @@ class QMW(QMainWindow):
         self.setWindowTitle("Statusbar")
         self.show()
 
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = QMW()
+    #ex = win()
+    ex2 = QMW()
     sys.exit(app.exec_())
